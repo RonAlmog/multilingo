c1ae9523fa583fd8eff92e074cbe54ef93f2cc13
Ron Almog <ron8132@gmail.com>
2024-03-17 12:00:27 -0400

    add mobile item to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,7 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
 
 type Props = {
   className?: string;
@@ -8,11 +11,21 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex flex-col bg-blue-500 h-full  lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2",
+        "flex flex-col h-full  lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2",
         className
       )}
     >
-      sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            MultiLingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+      </div>
     </div>
   );
 };
