c640c7869d16b26f71a75e3b36ae04fab48d9510
Ron Almog <ron8132@gmail.com>
2024-04-04 22:54:50 -0400

    more seeding of lessons, challenges

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -12,13 +12,79 @@ const main = async () => {
     console.log("Seeding database");
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+
     // insert
+
     await db.insert(schema.courses).values([
       { id: 1, title: "Spanish", imageSrc: "/es.svg" },
       { id: 2, title: "Italian", imageSrc: "/it.svg" },
       { id: 3, title: "French", imageSrc: "/fr.svg" },
       { id: 4, title: "Hebrew", imageSrc: "/hr.svg" },
     ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of spanish",
+        order: 1,
+      },
+    ]);
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: "Nouns",
+      },
+      {
+        id: 2,
+        unitId: 1,
+        order: 2,
+        title: "Verbs",
+      },
+    ]);
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        question: "Which one of these is the man?",
+        order: 1,
+      },
+    ]);
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
+
     console.log("Seeding finished");
   } catch (error) {
     console.error(error);
