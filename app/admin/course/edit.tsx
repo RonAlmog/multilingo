ba5ee03416f1776dd6a0276bfe909cc27a6d67c2
Ron Almog <ron8132@gmail.com>
2024-04-28 10:53:37 -0400

    add admin pages for course, units

--- app/admin/course/edit.tsx
+++ app/admin/course/edit.tsx
@@ -5,8 +5,18 @@ export const CourseEdit = () => {
     <Edit>
       <SimpleForm>
         <TextInput source="id" validate={[required()]} label="ID" />
-        <TextInput source="title" validate={[required()]} label="Title" />
-        <TextInput source="imageSrc" validate={[required()]} label="Image" />
+        <TextInput
+          source="title"
+          validate={[required()]}
+          label="Title"
+          fullWidth
+        />
+        <TextInput
+          source="imageSrc"
+          validate={[required()]}
+          label="Image"
+          fullWidth
+        />
       </SimpleForm>
     </Edit>
   );
