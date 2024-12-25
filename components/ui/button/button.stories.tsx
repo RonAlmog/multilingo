56e0a9e62b3a737f4dcc28d2c9db29e1fbff8eb5
Ron Almog <ron8132@gmail.com>
2024-12-25 16:34:09 -0500

    add argtypes and onClick fn

--- components/ui/button/button.stories.tsx
+++ components/ui/button/button.stories.tsx
@@ -1,46 +1,123 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./button";
+import { ComponentProps } from "react";
+import { fn } from "@storybook/test";
 
-const meta = {
+type StoryProps = ComponentProps<typeof Button> & { buttonText: string };
+const meta: Meta<StoryProps> = {
   title: "UI/Button",
   component: Button,
   parameters: {
     layout: "centered",
   },
 
-  args: { variant: "default", children: "Click Me" },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: [
+        "locked",
+        "default",
+        "primary",
+        "primaryOutline",
+        "secondary",
+        "secondaryOutline",
+        "danger",
+        "dangerOutline",
+        "super",
+        "superOutline",
+        "ghost",
+        "sidebar",
+        "sidebarOutline",
+      ],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["default", "sm", "lg", "icon", "rounded"],
+    },
+  },
+
+  // args: { variant: "default", children: "Click Me" },
+  args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: { buttonText: "Click Me Now", variant: "primary", size: "lg" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
 export const Primary: Story = {
-  args: { variant: "primary", size: "sm" },
+  args: { variant: "primary", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
 export const PrimaryOutline: Story = {
-  args: { variant: "primaryOutline" },
+  args: { variant: "primaryOutline", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
 export const Secondary: Story = {
-  args: { variant: "secondary" },
+  args: { variant: "secondary", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
 export const SecondaryOutline: Story = {
-  args: { variant: "secondaryOutline" },
+  args: { variant: "secondaryOutline", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
 export const Danger: Story = {
-  args: { variant: "danger" },
+  args: { variant: "danger", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
 export const DangerOutline: Story = {
-  args: { variant: "dangerOutline" },
+  args: { variant: "dangerOutline", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
+
+export const Super: Story = {
+  args: { variant: "super", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
+};
+export const SuperOutline: Story = {
+  args: { variant: "superOutline", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
+};
+
 export const Ghost: Story = {
-  args: { variant: "ghost" },
+  args: { variant: "ghost", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
 export const Sidebar: Story = {
-  args: { variant: "sidebar" },
+  args: { variant: "sidebar", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
 export const SidebarOutline: Story = {
-  args: { variant: "sidebarOutline" },
+  args: { variant: "sidebarOutline", size: "lg", buttonText: "Click Me" },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
 };
